afab4be2365d9a2598d30db342d8ecfc03cb650c
Danielle Martin <ordinaryworld2014@gmail.com>
2022-07-18 12:02:45 -0500

    updated daily-journal-server

---
+++ models/entryTag.py
@@ -0,0 +1,8 @@
+class EntryTag():
+    """Class for an entry tag
+    """
+
+    def __init__(self, id, entryId, tagId):
+        self.id = id
+        self.entryId = entryId,
+        self.tagId = tagId
